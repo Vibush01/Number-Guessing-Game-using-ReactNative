c5e3eccc692fe50e173034e9c83d3770b19faa17
vibush01 <126843555+vibush01@users.noreply.github.com>
2025-05-25 16:37:50 +0530

    made basic layout with input feild and add two button but styled yet

---
+++ screen/StartGameScreen.js
@@ -0,0 +1,47 @@
+import { TextInput, View, StyleSheet, Touchable,  } from "react-native";
+import PrimaryButton from "../components/PrimaryButton"; 
+
+function StartGameScreen(){
+    return (
+        <View style={styles.inputContainer}>
+            <TextInput 
+            style={styles.numberInput}
+            maxLength={2}
+            keyboardType="number-pad"
+            autoCapitalize="none"
+            autoCorrect={false}
+            />
+            <PrimaryButton>Reset</PrimaryButton>            
+            <PrimaryButton>Confirm</PrimaryButton>            
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    inputContainer:{
+        padding:16,
+        marginTop:100,
+        marginHorizontal: 24,
+        backgroundColor:"#72063c",
+        borderRadius:8,
+        elevation: 4,
+        shadowColor:'black',
+        shadowOffset:{width:0,height:2},
+        shadowOpacity:6,
+        shadowRadius:0.35,
+    },
+    numberInput:{
+        height:50,
+        width:50,
+        textAlign:"center",
+        fontSize:26,
+        borderBottomColor:"#ddb52f",
+        borderBottomWidth:2,
+        color:'#ddb52f',
+        marginVertical:8,
+        fontWeight:"bold"
+
+    }
+      
+});
+
+export default StartGameScreen;
